2b95016ad31d22e3a00cd37cf018a58502543587
Joe Shepherd <joeshepmedia@gmail.com>
2019-09-24 13:31:58 -0500

    update gitignore

--- hrapp/views/home.py
+++ hrapp/views/home.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 def home(request):
     if request.method == 'GET':
-        template = 'home.html'
+        template = 'registration/login.html'
         context = {}
 
         return render(request, template, context)
